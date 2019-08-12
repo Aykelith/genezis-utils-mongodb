2fc21f8a0eca33361fdae1739a9da4a3d82c0296
Alexandru Prisacariu <zalexxanderx@gmail.com>
2019-08-12 12:18:10 +0300

    Added the function createSingleDeleter to the return of RequestsUtils

--- RequestsUtils.js
+++ RequestsUtils.js
@@ -620,6 +620,7 @@ export default {
     createSingleSetter,
     createSingleAdder,
     createMultipleGetter,
+    createSingleDeleter,
 
     SingleGetterConfig: SingleGetterConfig,
     MultipleGetterConfig: MultipleGetterConfig,
